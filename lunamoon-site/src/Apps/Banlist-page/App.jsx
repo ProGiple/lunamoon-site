b7a7afc45feb06fc9bfccb5412b17aab10170e65
ProGiple <ghosthostgtop@gmail.com>
2024-11-10 21:53:46 +0300

    v1.1.2

--- lunamoon-site/src/Apps/Banlist-page/App.jsx
+++ lunamoon-site/src/Apps/Banlist-page/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import styles from './App.module.scss'
-import { BannedPlayerCell } from './element'
+import tableStyles from './table.module.scss'
 
 const sliceAmount = -25
 const sliceData = ({ varData=[], varPage }) => {
@@ -11,11 +11,16 @@ const sliceData = ({ varData=[], varPage }) => {
 }
 
 export function BanListPage() {
-    const data = [
+    const unSortedData = [
         { bannedNick: 'ProGiple', adminNick: 'Dakota_', reason: 'захотел', date: '09.11.2024', end: '08.10.2023', type: 'Мут' },
         { bannedNick: 'Dakota_', adminNick: 'JESTER', reason: 'нарушение правил', date: '10.11.2024', end: '10.12.2024', type: 'Бан' },
-        { bannedNick: 'JESTER', adminNick: 'ProGiple', reason: 'спам в чате', date: '11.11.2024', end: '11.12.2024', type: 'Мут' },
+        { bannedNick: 'JESTER', adminNick: 'ProGiple', reason: 'спам в чате', date: '11.11.2025', end: '11.12.2024', type: 'Мут' },
+        { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
     ];
+
+    const data = useMemo(() => {
+        return unSortedData.slice().sort((a, b) => new Date(b.date) - new Date(a.date))
+    }, [unSortedData])
 
     const [page, setPage] = useState(1)
     const [filterType, setFilterType] = useState("")
@@ -28,11 +33,12 @@ export function BanListPage() {
         setSearchTerm(event.target.value)
     }
 
-    const filteredData = data.filter((item) => {
-        const searchRegex = new RegExp(searchTerm, 'i');
-        return (!searchTerm || searchRegex.test(item.bannedNick) || searchRegex.test(item.adminNick)) &&
-               (!filterType || item.type === filterType);
-    })
+    const filteredData = useMemo(() => {
+        return data.filter((item) => 
+            (!searchTerm || item.bannedNick.toLowerCase().startsWith(searchTerm.toLowerCase())) &&
+            (!filterType || item.type === filterType)
+        );
+    }, [data, searchTerm, filterType])
 
     const getPages = Math.ceil(filteredData.length / -sliceAmount)
 
@@ -52,11 +58,12 @@ export function BanListPage() {
                         value={searchTerm}
                         onChange={handleSearchEnter} />
                 </div>
-                <h3>Страниц: {getPages}</h3>
-                <table className={styles.flex_item}>
-                    <thead>
-                        <tr>
-                            {filteredData.length !== 0 ?
+                <h3>Страница: {page}</h3>
+
+                {filteredData.length === 0 ? 'Ничего не найдено!' : <>
+                    <table className={styles.flex_item}>
+                        <thead>
+                            <tr>
                                 <>
                                     <th>Игрок</th>
                                     <th>Сотрудник</th>
@@ -64,30 +71,31 @@ export function BanListPage() {
                                     <th>Дата</th>
                                     <th>Истечение</th>
                                     <th>Тип</th>
-                                </> : <th>Ничего не найдено!</th>
-                            }
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {sliceData({varData: filteredData, varPage: page}).map((item, index) => (
-                            item && (
-                                <BannedPlayerCell
-                                    key={index}
-                                    bannedNick={item.bannedNick}
-                                    adminNick={item.adminNick}
-                                    reason={item.reason}
-                                    date={item.date}
-                                    end={item.end}
-                                    type={item.type} />
-                            )
-                        ))}
-                    </tbody>
-                </table>
-                <div className={styles.buttons}>
-                    {page !== 1 ? <button onClick={() => setPage(1)}>..1</button> : null}
-                    {page > 1 ? <button onClick={() => setPage(page - 1)}>«</button> : null}
-                    {getPages > page + 1 ? <button onClick={() => setPage(page + 1)}>»</button> : null}
-                </div>
+                                </>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sliceData({varData: filteredData, varPage: page}).map((item) => (
+                                item && (
+                                    <tr className={tableStyles.classic}>
+                                        <th>{item.bannedNick}</th>
+                                        <th>{item.adminNick}</th>
+                                        <th className={tableStyles.reason}>{item.reason}</th>
+                                        <th>{item.date}</th>
+                                        <th>{item.end}</th>
+                                        <th>{item.type}</th>
+                                    </tr>
+                                )
+                            ))}
+                        </tbody>
+                    </table>
+                    <div className={styles.buttons}>
+                        {page === 1 ? null : <button onClick={() => setPage(1)}>..1</button>}
+                        {page > 1 ? <button onClick={() => setPage(page - 1)}>« Назад</button> : null}
+                        {getPages > page ? <button onClick={() => setPage(page + 1)}>Вперёд »</button> : null}
+                        {page === getPages ? null : <button onClick={() => setPage(getPages)}>{getPages}..</button>}
+                    </div></>
+                }
         </div>
     )
 }
