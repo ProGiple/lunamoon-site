f91fb09a52ee3df14737d452e2fbfcae3e2ad012
ProGiple <ghosthostgtop@gmail.com>
2024-11-11 21:40:02 +0300

    v1.1.3

--- lunamoon-site/src/Apps/Banlist-page/App.jsx
+++ lunamoon-site/src/Apps/Banlist-page/App.jsx
@@ -16,6 +16,10 @@ export function BanListPage() {
         { bannedNick: 'Dakota_', adminNick: 'JESTER', reason: 'нарушение правил', date: '10.11.2024', end: '10.12.2024', type: 'Бан' },
         { bannedNick: 'JESTER', adminNick: 'ProGiple', reason: 'спам в чате', date: '11.11.2025', end: '11.12.2024', type: 'Мут' },
         { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
+        { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
+        { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
+        { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
+        { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
     ];
 
     const data = useMemo(() => {
@@ -25,12 +29,24 @@ export function BanListPage() {
     const [page, setPage] = useState(1)
     const [filterType, setFilterType] = useState("")
     const [searchTerm, setSearchTerm] = useState("")
+    const [tableHasOpacity, setTableHasOpacity] = useState(true)
   
     const handleFilterChange = (event) => {
-        setFilterType(event.target.value)
+        if (page !== 1) handleSetPage(1)
+        setFilterType(event.target.value)  
     }  
     const handleSearchEnter = (event) => {
+        if (page !== 1) handleSetPage(1)
         setSearchTerm(event.target.value)
+    }
+    const handleSetPage = (newPage) => {
+        setTableHasOpacity(false)
+        setTimeout(() => {
+            setPage(newPage)
+            setTimeout(() => {
+                setTableHasOpacity(true)
+            }, 15)
+        }, 325)
     }
 
     const filteredData = useMemo(() => {
@@ -44,7 +60,7 @@ export function BanListPage() {
 
     return (
         <div className={styles.block}>
-            <h3 className={`${styles.text} ${styles.flex_item}`}>Список наказаний</h3>
+            <h2 className={`${styles.text} ${styles.flex_item}`}>Список наказаний</h2>
                 <div className={styles.flex_item}>
                     <select value={filterType} onChange={handleFilterChange} className={styles.select_block}>
                         <option value="">Все типы</option>
@@ -58,22 +74,10 @@ export function BanListPage() {
                         value={searchTerm}
                         onChange={handleSearchEnter} />
                 </div>
-                <h3>Страница: {page}</h3>
+                <h3 className={`${styles.text} ${styles.flex_item}`}>Страница: {page}</h3>
 
                 {filteredData.length === 0 ? 'Ничего не найдено!' : <>
-                    <table className={styles.flex_item}>
-                        <thead>
-                            <tr>
-                                <>
-                                    <th>Игрок</th>
-                                    <th>Сотрудник</th>
-                                    <th>Причина</th>
-                                    <th>Дата</th>
-                                    <th>Истечение</th>
-                                    <th>Тип</th>
-                                </>
-                            </tr>
-                        </thead>
+                    <table className={`${styles.flex_item} ${tableHasOpacity ? null : tableStyles.opacity}`}>
                         <tbody>
                             {sliceData({varData: filteredData, varPage: page}).map((item) => (
                                 item && (
@@ -88,12 +92,24 @@ export function BanListPage() {
                                 )
                             ))}
                         </tbody>
+                        <thead>
+                            <tr>
+                                <>
+                                    <th>Игрок</th>
+                                    <th>Сотрудник</th>
+                                    <th>Причина</th>
+                                    <th>Дата</th>
+                                    <th>Истечение</th>
+                                    <th>Тип</th>
+                                </>
+                            </tr>
+                        </thead>
                     </table>
-                    <div className={styles.buttons}>
-                        {page === 1 ? null : <button onClick={() => setPage(1)}>..1</button>}
-                        {page > 1 ? <button onClick={() => setPage(page - 1)}>« Назад</button> : null}
-                        {getPages > page ? <button onClick={() => setPage(page + 1)}>Вперёд »</button> : null}
-                        {page === getPages ? null : <button onClick={() => setPage(getPages)}>{getPages}..</button>}
+                    <div className={`${styles.buttons} ${page === 1 || page === getPages || page === 2 || page === getPages - 1 ? (tableHasOpacity ? null : tableStyles.opacity) : null}`}>
+                        {page === 1 ? null : <button onClick={() => handleSetPage(1)}>..1</button>}
+                        {page > 1 ? <button onClick={() => handleSetPage(page - 1)}>« Назад</button> : null}
+                        {getPages > page ? <button onClick={() => handleSetPage(page + 1)}>Вперёд »</button> : null}
+                        {page === getPages ? null : <button onClick={() => handleSetPage(getPages)}>{getPages}..</button>}
                     </div></>
                 }
         </div>
