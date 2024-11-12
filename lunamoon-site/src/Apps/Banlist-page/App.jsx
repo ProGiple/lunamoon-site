c652cd7797822aaa33bbdf22d9b26cdd766c06a0
ProGiple <ghosthostgtop@gmail.com>
2024-11-12 20:45:05 +0300

    v1.1.4

--- lunamoon-site/src/Apps/Banlist-page/App.jsx
+++ lunamoon-site/src/Apps/Banlist-page/App.jsx
@@ -20,6 +20,7 @@ export function BanListPage() {
         { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
         { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
         { bannedNick: 'strelka_', adminNick: 'Dakota_', reason: 'даунизм', date: '11.11.2024', end: '24.05.2026', type: 'Варн'},
+        { bannedNick: 'ProGiple', adminNick: 'Dakota_', reason: 'захотел', date: '09.11.2024', end: '08.10.2023', type: 'Мут' },
     ];
 
     const data = useMemo(() => {
@@ -27,14 +28,21 @@ export function BanListPage() {
     }, [unSortedData])
 
     const [page, setPage] = useState(1)
-    const [searchTerm, setSearchTerm] = useState("")
+    const [searchPlayer, setSearchPlayer] = useState("")
+    const [searchAdmin, setSearchAdmin] = useState("")
+
     const [tableHasOpacity, setTableHasOpacity] = useState(true)
     const [checkboxValues, setCheckboxValues] = useState({ mutes: false, warns: false, bans: false });
  
-    const handleSearchEnter = (event) => {
+    const handleSearchPlayer = (event) => {
         if (page !== 1) handleSetPage(1)
-        setSearchTerm(event.target.value)
+        setSearchPlayer(event.target.value);
     }
+    const handleSearchAdmin = (event) => {
+        if (page !== 1) handleSetPage(1)
+        setSearchAdmin(event.target.value);
+    }
+
     const handleSetPage = (newPage) => {
         setTableHasOpacity(false)
         setTimeout(() => {
@@ -46,6 +54,7 @@ export function BanListPage() {
     }
     const handleCheckboxChange = (event) => {
         const { name, checked } = event.target;
+        if (page !== 1) handleSetPage(1)
         setCheckboxValues(prevState => ({
             ...prevState,
             [name]: checked
@@ -54,10 +63,10 @@ export function BanListPage() {
 
     const filteredData = useMemo(() => {
         return data.filter((item) => 
-            (!searchTerm || item.bannedNick.toLowerCase().startsWith(searchTerm.toLowerCase()))
+            (!searchPlayer || item.bannedNick.toLowerCase().startsWith(searchPlayer.toLowerCase())) && item.adminNick.toLowerCase().startsWith(searchAdmin.toLowerCase())
             && ((!checkboxValues.bans && !checkboxValues.mutes && !checkboxValues.warns) || (checkboxValues.bans && item.type == 'Бан') || (checkboxValues.mutes && item.type == 'Мут') || (checkboxValues.warns && item.type == 'Варн'))
         );
-    }, [data, searchTerm, checkboxValues])
+    }, [data, searchPlayer, checkboxValues])
 
     const getPages = Math.ceil(filteredData.length / -sliceAmount)
 
@@ -65,33 +74,41 @@ export function BanListPage() {
         <div className={styles.block}>
             <h2 className={`${styles.text} ${styles.flex_item}`}>Список наказаний</h2>
                 <div className={styles.flex_item}>
-                    <div className={styles.checkBoxes}>
-                        <span className={styles.text}>Показать:</span>
-                        <input type="checkbox" name="warns" checked={checkboxValues.checkbox1} onChange={handleCheckboxChange} />
+                    <div className={styles.text}>
+                        <span className={styles.text}>Показать: </span>
+                        <input type="checkbox" name="warns" checked={checkboxValues.warns} className={styles.checkBox} onChange={handleCheckboxChange} />
                         <label htmlFor="warns">Варны</label>
 
-                        <input type="checkbox" name="mutes" checked={checkboxValues.checkbox2} onChange={handleCheckboxChange} />
+                        <input type="checkbox" name="mutes" checked={checkboxValues.mutes} className={styles.checkBox} onChange={handleCheckboxChange} />
                         <label htmlFor="mutes">Муты</label>
 
-                        <input type="checkbox" name="bans" checked={checkboxValues.checkbox2} onChange={handleCheckboxChange} />
+                        <input type="checkbox" name="bans" checked={checkboxValues.bans} className={styles.checkBox} onChange={handleCheckboxChange} />
                         <label htmlFor="bans">Баны</label>
                     </div>
-                    <input
-                        type="text"
-                        placeholder="Поиск по игрокам..."
-                        value={searchTerm}
-                        onChange={handleSearchEnter} />
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Поиск по игрокам..."
+                            value={searchPlayer}
+                            onChange={handleSearchPlayer} />
+                        <input
+                            type="text"
+                            placeholder="Поиск по сотрудникам..."
+                            value={searchAdmin}
+                            onChange={handleSearchAdmin} />
+                        <img src='/mark1.png' onClick={() => {setSearchAdmin(""), setSearchPlayer(""), setCheckboxValues({ mutes:false, warns:false, bans:false })}} className={styles.mark} />
+                    </div>
                 </div>
                 <h3 className={`${styles.text} ${styles.flex_item}`}>Страница: {page} / {getPages}</h3>
 
-                {filteredData.length === 0 ? 'Ничего не найдено!' : <>
+                {filteredData.length === 0 ? <h3 className={`${styles.text} ${styles.flex_item}`}>Ничего не найдено!</h3> : <>
                     <table className={`${styles.flex_item} ${tableHasOpacity ? null : tableStyles.opacity}`}>
                         <tbody>
-                            {sliceData({varData: filteredData, varPage: page}).map((item) => (
+                            {sliceData({varData: filteredData, varPage: page}).map((item, index) => (
                                 item && (
-                                    <tr className={tableStyles.classic}>
-                                        <th>{item.bannedNick}</th>
-                                        <th>{item.adminNick}</th>
+                                    <tr className={tableStyles.classic} key={index}>
+                                        <th className={tableStyles.inline}><img src={`https://minotar.net/helm/${item.bannedNick}/250`} /> {item.bannedNick}</th>
+                                        <th className={tableStyles.inline}><img src={`https://minotar.net/helm/${item.adminNick}/250`} /> {item.adminNick}</th>
                                         <th className={tableStyles.reason}>{item.reason}</th>
                                         <th>{item.date}</th>
                                         <th>{item.end}</th>
